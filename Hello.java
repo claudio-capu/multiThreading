b640546f421a8e891405d08755e96e03f9165595
Claudio-Caputo-Peano-4A <56753336+claudio-capu@users.noreply.github.com>
2019-11-16 09:27:40 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package provaThreads;
+package provaThread;
 
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author mortera.mattjairoort
+ * @author claudio.caputo
  */
 public class Hello extends Thread {
 
