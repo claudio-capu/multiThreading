7a10e745e90ef6a7141a675790ad0aae3bfea8ae
Claudio-Caputo-Peano-4A <56753336+claudio-capu@users.noreply.github.com>
2019-11-16 09:26:45 +0100

    Update Say.java

--- Say.java
+++ Say.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package provaThreads;
+package provaThread;
 
 /**
  *
- * @author mortera.mattjairoort
+ * @author claudio.caputo
  */
 public class Say extends Thread {
 
